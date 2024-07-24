179c3b6de9650b77a548d7eb4645f6fe26c40204
GBGreenBravo <gbgreenbravo@gmail.com>
2024-07-24 17:15:09 +0900

    chore: add another code

--- BOJ/Silver/11055_가장큰증가하는부분수열.py
+++ BOJ/Silver/11055_가장큰증가하는부분수열.py
@@ -12,3 +12,20 @@ for i in range(n):
             tmp_mx = max(tmp_mx, mx_lst[j])
     mx_lst[i] = arr[i] + tmp_mx
 print(max(mx_lst))
+
+
+# 뒤집을 필요 없이 앞에서부터 구해도 됨.
+"""
+N = int(input())
+arr = list(map(int, input().split()))
+DP = arr[:]
+
+ans = 0
+
+for i in range(1, N):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            DP[i] = max(DP[j] + arr[i], DP[i])
+
+print(max(DP))
+"""
