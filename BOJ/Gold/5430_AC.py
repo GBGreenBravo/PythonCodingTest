05639d82c46c4fb33db16171dab4fb407c64faf8
GBGreenBravo <gbgreenbravo@gmail.com>
2024-07-29 12:50:01 +0900

    BOJ: 5430_AC 풀이 추가

--- BOJ/Gold/5430_AC.py
+++ BOJ/Gold/5430_AC.py
@@ -36,3 +36,42 @@ for _ in range(t):
         print("[", end="")
         print(*nums if direction_right else nums[::-1], sep=',', end="")
         print("]")
+
+
+# deque를 이용한 풀이
+# 삭제하는 방식을 list의 요소 삭제에서 deque의 pop/popleft로 바꾸니 소요 시간 1/10로 줆.
+"""
+from collections import deque
+
+t = int(input())
+for _ in range(t):
+    p = list(str(input()))
+    n = int(input())
+    nums = str(input())
+    nums = deque([i for i in nums[1:-1].split(",")] if "," in nums else [int(nums[1])] if nums != "[]" else [])
+
+    start, end = 0, len(nums) - 1
+    direction_right = True
+    throw_error = False
+
+    for pp in p:
+        if pp == 'R':
+            direction_right = not direction_right
+        elif pp == 'D':
+            if len(nums) == 0:
+                throw_error = True
+                break
+            if direction_right:
+                nums.popleft()
+                end -= 1
+            else:
+                nums.pop()
+                end -= 1
+
+    if throw_error:
+        print("error")
+    else:
+        print("[", end="")
+        print(*nums if direction_right else reversed(nums), sep=',', end="")
+        print("]")
+"""
