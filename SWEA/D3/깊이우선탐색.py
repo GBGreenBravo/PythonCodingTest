11e1c3e4ad06dc379b9fd5b492474adf513c34a9
GBGreenBravo <gbgreenbravo@gmail.com>
2024-07-30 10:58:43 +0900

    SWEA: Stack1_연습문제_2: 깊이우선탐색 (제출용)

---
+++ SWEA/D3/깊이우선탐색.py
@@ -0,0 +1,40 @@
+# 20240730
+# 12:48
+
+def dfs(start_node):
+    visited = [0] * (v + 1)
+    visited[start_node] = 1
+    stk = []
+    answer = [start_node]
+    now = start_node
+
+    while True:
+        for n in lines[now]:
+            if visited[n] == 0:
+                visited[n] = 1
+                answer.append(n)
+                stk.append(now)
+                now = n
+                break
+        else:
+            if stk:
+                now = stk.pop()
+            else:
+                break
+    return answer
+
+
+T = int(input())
+for test_case in range(1, T + 1):
+    v, e = map(int, input().split())
+    lines = [[] for _ in range(v + 1)]
+
+    for _ in range(e):
+        a, b = map(int, input().split())
+        lines[a].append(b)
+        lines[b].append(a)
+
+    for line in lines:
+        line.sort()
+
+    print(f"#{test_case}", *dfs(1))
