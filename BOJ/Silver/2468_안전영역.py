e978658b992632d978d52189b086810f1e1fcad7
GBGreenBravo <gbgreenbravo@gmail.com>
2024-08-01 15:38:40 +0900

    BOJ: 2468_안전 영역

--- BOJ/Silver/2468_안전영역.py
+++ BOJ/Silver/2468_안전영역.py
@@ -85,3 +85,50 @@ for rain in range(1, max([max(i) for i in area])):
 
 print(mx)
 """
+
+
+# 202040801
+# 12:40
+# 1 / 1
+# BFS 풀이
+"""
+from collections import deque
+
+n = int(input())
+area = [list(map(int, input().split())) for _ in range(n)]
+
+mx = max([max(row) for row in area])
+rain = 0
+answer = 1
+
+
+def bfs(sy, sx):
+    queue = deque()
+    queue.append((sy, sx))
+    visited[sy][sx] = 1
+
+    while queue:
+        y, x = queue.popleft()
+        for dy, dx in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            ny, nx = y + dy, x + dx
+            if 0 <= ny < n and 0 <= nx < n and area[ny][nx] > rain and not visited[ny][nx]:
+                visited[ny][nx] = 1
+                queue.append((ny, nx))
+
+
+while rain < mx - 1:  # 최대 높이 이상의 비면 종료.
+    rain += 1
+
+    visited = [[0] * n for _ in range(n)]
+
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if area[i][j] > rain and not visited[i][j]:  # 비 높이 초과이고, 체크되지 않은 좌표라면 BFS
+                bfs(i, j)
+                cnt += 1
+
+    answer = max(answer, cnt)
+
+print(answer)
+"""
