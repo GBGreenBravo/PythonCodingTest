5fac6c67ae74861d835c74bc00276bddec7665f3
GBGreenBravo <gbgreenbravo@gmail.com>
2024-08-12 14:45:39 +0900

    BOJ: 1072_寃뚯엫

--- BOJ/Silver/1072_게임.py
+++ BOJ/Silver/1072_게임.py
@@ -1,28 +1,33 @@
+# 20240812
+# 1:54:09
+# 25:48
+# 1 / 14
+
+
+# y = 29, x = 50 면
+# int(y / x * 100) 에서는 57 반환하고
+# 100 * y // x 에서는 58 반환한다.
+# 부동소수점으로 인한 알려진 에러. 따라서 이러한 경우에는 정수끼리 계산한 아래의 방식을 활용하는 게 정확하다.
+
 def cal(num):
-    return int(((y + num) / (x + num)) * 100)
+    return 100 * (y + num) // (x + num)  # 이 부분을 return int((y + num) / (x + num) * 100)으로 작성했어서 틀림.
 
 
 x, y = map(int, input().split())
+now = cal(0)
 
-if x == y or cal(0) >= 99:
+if now >= 99:
     print(-1)
 else:
-    now = cal(0)
-
-    start = 0
-    # end = 1_000_000_000 ** 2
-    end = int(x ** 2 / (99 * x - 100 * y)) + 1
-    while end - start > 0:
-        middle = (start + end) // 2
-
+    left = 0
+    right = x ** 2
+    while left < right:
+        middle = int((left + right) // 2)
         cal_middle = cal(middle)
 
-        if cal_middle == now:
-            start = middle + 1
+        if cal_middle <= now:
+            left = middle + 1
         elif cal_middle > now:
-            end = middle
+            right = middle
 
-    print(start)
-
-# 1:54:09
-
+    print(left)
