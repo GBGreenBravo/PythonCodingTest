bd1137d5e753239356112f11f1e4dd91eb45be40
GBGreenBravo <gbgreenbravo@gmail.com>
2024-09-13 20:20:39 +0900

    BOJ: 3019_테트리스

---
+++ BOJ/Gold/3019_테트리스.py
@@ -0,0 +1,44 @@
+# 20240913
+# 26:35
+# 1 / 1
+
+shapes = (None,
+          ((4, ), (1, 1, 1, 1)),
+          ((2, 2),),
+          ((2, 2, 1), (2, 3)),
+          ((1, 2, 2), (3, 2)),
+          ((2, 2, 2), (1, 2, 1), (2, 3), (3, 2)),
+          ((2, 2, 2), (3, 3), (2, 1, 1), (1, 3)),
+          ((2, 2, 2), (3, 3), (1, 1, 2), (3, 1))
+          )
+
+c, p = map(int, input().split())
+height = 105
+area = [[0] * c for _ in range(height)]
+input_bottoms = list(map(int, input().split()))
+for col in range(c):
+    for r in range(input_bottoms[col]):
+        area[-1 - r][col] = 1
+
+case_cnt = 0
+
+falling_shapes = shapes[p]
+for falling in falling_shapes:
+    for col in range(0, c - len(falling) + 1):
+        now_row = 0
+        while now_row < height:
+            touch = 0
+            for d in range(len(falling)):
+                depth = falling[d]
+                if now_row + depth >= height:
+                    touch += 1
+                elif area[now_row + depth][col + d]:
+                    touch += 1
+            if not touch:
+                now_row += 1
+                continue
+            if touch == len(falling):
+                case_cnt += 1
+            break
+
+print(case_cnt)
