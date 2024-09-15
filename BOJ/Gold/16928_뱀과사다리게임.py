dce60f30adbf2869b280fa5a2043bcac8bb70ba3
GBGreenBravo <gbgreenbravo@gmail.com>
2024-09-15 20:26:38 +0900

    BOJ: 16928_뱀과 사다리 게임

--- BOJ/Gold/16928_뱀과사다리게임.py
+++ BOJ/Gold/16928_뱀과사다리게임.py
@@ -1,7 +1,51 @@
+# 20240915
+# 08:54
+# 1 / 2
+
+# BFS 활용할 때, visited 필요 여부 항상 체크하자..!
+
+from collections import deque
+
+
+def bfs():
+    queue = deque()
+    queue.append((1, 0))
+
+    while queue:
+        now, cnt = queue.popleft()
+        for i in range(6, 0, -1):
+            nex = now + i
+            if nex > 100 or visited[nex]:
+                continue
+            if nex == 100:
+                print(cnt + 1)
+                return
+            if ladders[nex]:
+                nex = ladders[nex]
+            elif snakes[nex]:
+                nex = snakes[nex]
+            queue.append((nex, cnt + 1))
+            visited[nex] = 1
+
+
+n, m = map(int, input().split())
+ladders = [0] * 101
+snakes = [0] * 101
+for _ in range(n):
+    a, b = map(int, input().split())
+    ladders[a] = b
+for _ in range(m):
+    a, b = map(int, input().split())
+    snakes[a] = b
+visited = [0] * 101
+visited[1] = 1
+bfs()
+
+
 # 20240818
 # 10:54
 # 1 / 1
-
+"""
 from heapq import heappop, heappush
 
 n, m = map(int, input().split())
@@ -30,3 +74,4 @@ while queue:
         if next <= 100 and visited[next] > time + 1:  # 100칸 이내에 있고, 기존보다 더 적은 시간으로 도달 가능하다면
             visited[next] = time + 1
             heappush(queue, (time + 1, next))
+"""
