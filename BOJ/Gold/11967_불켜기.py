6cb00ba58d54d69d0e251427900f724283332b7c
GBGreenBravo <gbgreenbravo@gmail.com>
2024-09-29 15:09:43 +0900

    BOJ: 11967_불 켜기

--- BOJ/Gold/11967_불켜기.py
+++ BOJ/Gold/11967_불켜기.py
@@ -50,3 +50,56 @@ while queue:
     queue = next_queue
 
 print(sum(map(sum, visited)) + len(set(possibles)))
+
+
+# next_possibles/next_queue에 중복된 좌표 안 담기도록 리팩토링
+"""
+direction = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+
+def oob(yy, xx):
+    return yy < 0 or n <= yy or xx < 0 or n <= xx
+
+
+n, m = map(int, input().split())
+connected = [[[] for _ in range(n)] for _ in range(n)]
+for _ in range(m):
+    aa, bb, cc, dd = map(int, input().split())
+    connected[aa - 1][bb - 1].append((cc - 1, dd - 1))
+
+visited = [[0] * n for _ in range(n)]
+visited[0][0] = 1
+
+queue = []
+queue.append((0, 0))
+
+possibles = []
+
+while queue:
+    for y, x in queue:
+        for ny, nx in connected[y][x]:
+            if not visited[ny][nx]:
+                possibles.append((ny, nx))
+
+    next_possibles = []
+    next_queue = []
+    for y, x in possibles:
+        if visited[y][x]:
+            continue
+        for dy, dx in direction:
+            ny, nx = y + dy, x + dx
+            if oob(ny, nx) or not visited[ny][nx]:
+                continue
+            visited[y][x] = 1
+            next_queue.append((y, x))
+            break
+        else:
+            next_possibles.append((y, x))
+            continue
+
+
+    possibles = next_possibles
+    queue = next_queue
+
+print(sum(map(sum, visited)) + len(set(possibles)))
+"""
