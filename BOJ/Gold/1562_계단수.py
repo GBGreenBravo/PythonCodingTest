2f1cb2af740997fdc17982a279f2b7088106d102
GBGreenBravo <gbgreenbravo@gmail.com>
2024-12-03 11:15:08 +0900

    BOJ: 1562_계단수

--- BOJ/Gold/1562_계단수.py
+++ BOJ/Gold/1562_계단수.py
@@ -2,25 +2,15 @@
 # 26:55
 # 1 / 1
 
-from itertools import combinations
-
 N = int(input())
 M = 1_000_000_000
 
 now = dict()
-for i in range(1, 11):
-    for comb in combinations([j for j in range(10)], i):
-        for k in range(10):
-            now[tuple(list(comb) + [k])] = 0
 for i in range(1, 10):
     now[(i, i,)] = 1
 
 for _ in range(N - 1):
     next = dict()
-    for i in range(1, 11):
-        for comb in combinations([j for j in range(10)], i):
-            for k in range(10):
-                next[tuple(list(comb) + [k])] = 0
 
     for comb, value in now.items():
         if not value:
@@ -28,19 +18,19 @@ for _ in range(N - 1):
 
         if comb[-1] == 0:
             key = tuple(sorted(list(set(comb) | {1}))) + (1,)
-            next[key] = (next[key] + value) % M
+            next[key] = (next.get(key, 0) + value) % M
         elif comb[-1] == 9:
             key = tuple(sorted(list(set(comb) | {8}))) + (8,)
-            next[key] = (next[key] + value) % M
+            next[key] = (next.get(key, 0) + value) % M
         else:
             key = tuple(sorted(list(set(comb) | {comb[-1] - 1}))) + (comb[-1] - 1,)
-            next[key] = (next[key] + value) % M
+            next[key] = (next.get(key, 0) + value) % M
             key = tuple(sorted(list(set(comb) | {comb[-1] + 1}))) + (comb[-1] + 1,)
-            next[key] = (next[key] + value) % M
+            next[key] = (next.get(key, 0) + value) % M
 
     now = next
 
 answer = 0
 for i in range(10):
-    answer += now[(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) + (i,)]
+    answer += now.get((0, 1, 2, 3, 4, 5, 6, 7, 8, 9) + (i,), 0)
 print(answer % M)
