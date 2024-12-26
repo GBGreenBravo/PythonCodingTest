54031e0f975c3139ace2292b00d5b0509c67cc66
GBGreenBravo <gbgreenbravo@gmail.com>
2024-12-26 15:05:02 +0900

    BOJ: 20304_비밀번호 제작

--- BOJ/Platinum/20304_비밀번호제작.py
+++ BOJ/Platinum/20304_비밀번호제작.py
@@ -4,17 +4,14 @@
 
 N = int(input())
 M = int(input())
-total_len = 0
-while N > 2**total_len:
-    total_len += 1
-total_len += 1
+total_len = len(bin(N)) - 2
 visited = [total_len] * (N + 1)
 starts = list(map(int, input().split()))
 for start in starts:
     visited[start] = 0
-queue = set(starts)
+queue = starts
 while queue:
-    next_queue = set()
+    next_queue = []
     for now in queue:
         distance = visited[now]
         for i in range(total_len):
@@ -23,7 +20,7 @@ while queue:
                 continue
             if distance + 1 < visited[nex]:
                 visited[nex] = distance + 1
-                next_queue.add(nex)
+                next_queue.append(nex)
     queue = next_queue
 
 print(max(visited))
